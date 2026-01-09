be730186f33801d863df860c2d1c278e95168852
Anila Nawaz <anilanawaz531@gmail.com>
2026-01-09 09:39:12 +0500

    improved solution

--- deep/deep.py
+++ deep/deep.py
@@ -1,7 +1,11 @@
+
 answer = input("What is the Answer to the Great Question of Life, the Universe, and Everything?  ")
 
+#removing any extra spaces before and after the value user inputs and converting it to lowercase to deal with case sensitivity
+answer = answer.strip().lower()
+
 match(answer):
-    case "forty two" | "forty-two" | "FORTY TWO" | "FORTY-TWO" | "42":
+    case "forty two" | "forty-two" | "42":
         print("Yes")
     case _:
         print("No")
